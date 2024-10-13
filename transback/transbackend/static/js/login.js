78804c4190c9bfec8ca4c2312b5eec254fc03300
batuhan <batuhan.tanir00@gmail.com>
2024-10-13 17:19:38 +0300

    added login/ reegister page ui

---
+++ transback/transbackend/static/js/login.js
@@ -0,0 +1,9 @@
+function togglePanel(toggle) {
+  const container = document.getElementById("container");
+  if (toggle) container.classList.add("right-panel-active");
+  else container.classList.remove("right-panel-active");
+}
+
+document.getElementById("sign-in").addEventListener("click", function () {
+    
+});
