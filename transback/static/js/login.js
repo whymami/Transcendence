6170e8d710dd73afe1c1629c7c490e223053d100
batuhan <batuhan.tanir00@gmail.com>
2024-11-23 18:19:50 +0300

    2fa

--- transback/static/js/login.js
+++ transback/static/js/login.js
@@ -21,24 +21,30 @@
 }
 
 async function login() {
-  const email = document.getElementById("email").value.trim();
+  const username = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value.trim();
+  const loginBtn = document.getElementsByClassName("login-btn");
 
-  const emailError = document.getElementById("emailError");
+  const usernameError = document.getElementById("usernameError");
   const passwordError = document.getElementById("passwordError");
   const generalError = document.getElementById("generalError");
 
-  emailError.textContent = "";
+  usernameError.textContent = "";
   passwordError.textContent = "";
   generalError.textContent = "";
 
   let isValid = true;
 
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailPattern.test(email)) {
-    emailError.textContent = "Please enter a valid email address.";
+  if(username.length < 4){
+    usernameError.textContent = "Username must be at least 4 characters long.";
     isValid = false;
   }
+
+  // const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // if (!emailPattern.test(email)) {
+  //   emailError.textContent = "Please enter a valid email address.";
+  //   isValid = false;
+  // }
 
   if (password.length < 6) {
     passwordError.textContent = "Password must be at least 4 characters long.";
@@ -48,28 +54,33 @@ async function login() {
   if (!isValid) return;
 
   try {
+    loginBtn.textContent = "Loading...";
+    loginBtn.disabled = true;
     const response = await fetch('/api/login/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ email, password })
+      body: JSON.stringify({ username, password })
     });
 
     const data = await response.json();
     if (!response.ok) {
-      showToast('error', data?.error);
+      showToast('error', data?.message);
       return;
     }
     
     setCookie('access_token', data.access, 1);
     setCookie('refresh_token', data.refresh, 1);
 
-    showToast('success', data.massage);
-    history.pushState({}, "", "/");
+    showToast('success', data.message);
+    history.pushState({}, "", "/2fa");
     urlLocationHandler();
     pullHeader(true);
 
   } catch (error) {
-    console.error('Error:', error);
+    console.error('Error:', error?.message);
     showToast('error', 'An error occurred while logging in.');
+  } finally {
+    loginBtn.textContent = "Login";
+    loginBtn.disabled = false;
   }
 }
