ea933638d72e0e59d48a02e29371493e9605015e
bizarreuproar <emirys774@gmail.com>
2024-11-23 18:36:44 +0300

    game settings

--- transback/transback/settings.py
+++ transback/transback/settings.py
@@ -64,11 +64,8 @@ REST_FRAMEWORK = {
 }
 
 CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-        },
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
     },
 }
 
