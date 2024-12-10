1b2828c7595f87bd3925d9d82971f149b2eb9b2b
bizarreuproar <emirys774@gmail.com>
2024-12-10 19:57:41 +0300

    delete createsuperuser from manage.py

--- transback/manage.py
+++ transback/manage.py
@@ -2,22 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from transback.vaultclient import get_secret 
-
-
-def create_superUser():
-    try:
-        User.objects.get(username=get_secret('DB_USER'))
-        print("Superuser already exists.")
-    except User.DoesNotExist:
-        # Eğer 'admin' yoksa, yeni superuser oluşturulacak
-        User.objects.create_superuser(get_secret('DB_USER'), get_secret('DEFAULT_FROM_EMAIL'), get_secret('DB_PASSWORD'))
-        print("Superuser created successfully.")
 
 def main():
-
-    create_superUser()
-
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transback.settings')
     try:
